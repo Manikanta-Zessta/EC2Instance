b28c409ce56e0e1b74c745a06611318b387c2a5c
Manikanta-Zessta <manikanta.kakileti@zessta.com>
2023-05-02 17:31:36 +0530

    Add server file

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+  res.send('<h1>Helloo World</h2><h3>Good Morning</h3>');
+});
+app.listen(5001, () => {
+  console.log('connected with port 5001');
+});
